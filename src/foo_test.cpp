a4b3fb8ecf926742670a836b9a10ae07e5d12090
NathanLBCooper <nathan.lb.cooper@outlook.com>
2019-02-19 09:30:44 +0100

    Switch to cmake, disable tests and rabbit imports for now

--- src/foo_test.cpp
+++ src/foo_test.cpp
@@ -1,9 +1,9 @@
-#include "../catch/catch.hpp"
-#include "consoleLogger.hpp"
+// #include "../catch/catch.hpp"
+// #include "consoleLogger.hpp"
 
-using namespace RabbitMqExample;
+// using namespace RabbitMqExample;
 
-TEST_CASE( "Exampletest", "[Exampletest]" ) {
-    ConsoleLogger logger;
-    REQUIRE( 1 == 2 );
-}
+// TEST_CASE( "Exampletest", "[Exampletest]" ) {
+//     ConsoleLogger logger;
+//     REQUIRE( 1 == 2 );
+// }
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
-#include <amqp.h>
-#include <amqp_tcp_socket.h>
+// #include <amqp.h>
+// #include <amqp_tcp_socket.h>
 
 #include "consoleLogger.hpp"
 
@@ -7,11 +7,12 @@ using namespace RabbitMqExample;
 
 int main(){
     ConsoleLogger logger;
-    
-    amqp_connection_state_t conn;
+    logger.log("hello from cmake");
 
-    conn = amqp_new_connection();
-    amqp_destroy_connection(conn);
+    // amqp_connection_state_t conn;
+
+    // conn = amqp_new_connection();
+    // amqp_destroy_connection(conn);
 
     return 1;
 }
